efb00028df9d6151ea40167452967f66b2e21eb4
Ethan Chen <intervigil@gmail.com>
2018-10-02 13:51:03 +0200

    prague: Use Soong namespace to avoid device build conflicts
    Change-Id: Id04f464102264b6f2e8d49b39389c7e346613b39

--- device.mk
+++ device.mk
@@ -4,5 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
 # Call the proprietary setup
 $(call inherit-product, vendor/huawei/prague/prague-vendor.mk)
