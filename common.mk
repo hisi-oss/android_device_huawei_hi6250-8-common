b6ffafef8e78a911cb874037c4411a379c2349d6
Giovanni Ricca <giovanniricca@proton.me>
2023-02-19 17:31:37 +0100

    hi6250-8-common: overlay-lineage: Declare hw keys
    Change-Id: I9326af9e0b81e0cebd3dd9940a6520628d0c1cfa

--- common.mk
+++ common.mk
@@ -9,7 +9,8 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
