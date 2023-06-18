3535f9c7c30404a8d1d6369df2471e993fd1a33a
dianlujitao <dianlujitao@lineageos.org>
2023-06-18 21:44:55 +0000

    prague: Build a dummy `android.hidl.base@1.0`
    Change-Id: I4593b11d96c3e8daa57a5eef6b3fb2964ce66623

--- device.mk
+++ device.mk
@@ -71,6 +71,8 @@ PRODUCT_COPY_FILES += \
 
 # Binder
 PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.base@1.0.vendor \
     android.hidl.allocator@1.0.vendor \
     android.hidl.memory@1.0.vendor \
     libhidltransport \
