56d7e3d831e071fb14510a2c8d7044b636b61a3a
A2L5E0X1 <alexed25@protonmail.com>
2023-06-19 18:54:54 +0200

    prague: hack to bypass xml checks of `native_packages.xml`
    - This is required because `native_packages.xml` is not a real file but
      a binary file used by `teecd`.
    
    Change-Id: I11b250eb5e146422ac4df96feb1eb7355365f033

--- Android.mk
+++ Android.mk
@@ -10,4 +10,11 @@ ifeq ($(TARGET_DEVICE),prague)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
+NATIVE_PACKAGES_FIXUP := $(TARGET_OUT_VENDOR)/etc/native_packages.xml
+$(NATIVE_PACKAGES_FIXUP): $(TARGET_OUT_VENDOR)/etc/native_packages.bin
+	@echo "Move vendor native_packages.bin to native_packages.xml"
+	$(hide) mv $(TARGET_OUT_VENDOR)/etc/native_packages.bin $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(NATIVE_PACKAGES_FIXUP)
+
 endif
