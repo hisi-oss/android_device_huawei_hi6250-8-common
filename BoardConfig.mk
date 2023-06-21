9498dd0e40cb8f6aff9099b01411e7c4919c4784
Giovanni Ricca <giovanniricca@proton.me>
2023-06-21 19:51:29 +0200

    prague: Add kernel flags
    * Extracted with unpack_bootimg.py [1]
    
    $ python3 mkbootimg.py --boot_img boot.img
    boot magic: ANDROID!
    kernel_size: 11642147
    kernel load address: 0x00480000
    ramdisk size: 0
    ramdisk load address: 0x08000000
    second bootloader size: 0
    second bootloader load address: 0x01300000
    kernel tags load address: 0x07e00000
    page size: 2048
    os version: 8.0.0
    os patch level: 2018-09
    boot image header version: 0
    product name:
    command line args: loglevel=4 coherent_pool=512K page_tracker=on slub_min_objects=12 unmovable_isolate1=2:192M,3:224M,4:256M printktimer=0xfff0a000,0x534,0x538 androidboot.selinux=enforcing buildvariant=user
    additional command line args:
    
    [1] https://android.googlesource.com/platform/system/tools/mkbootimg/+/refs/heads/master/unpack_bootimg.py
    
    Change-Id: I929eca48a4a85841131ecede7180d5e88df0d0f8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,5 +21,22 @@ TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := generic
 TARGET_2ND_CPU_VARIANT_RUNTIME := generic
 
+# Kernel
+BOARD_KERNEL_BASE := 0x00480000
+BOARD_KERNEL_IMAGE_NAME := Image.gz
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_CMDLINE := loglevel=4 coherent_pool=512K page_tracker=on slub_min_objects=12 unmovable_isolate1=2:192M,3:224M,4:256M printktimer=0xfff0a000,0x534,0x538
+
+BOARD_KERNEL_TAGS_OFFSET := 0x07e00000
+BOARD_RAMDISK_OFFSET := 0x08000000
+BOARD_SECOND_OFFSET := 0x01300000
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --second_offset $(BOARD_SECOND_OFFSET)
+
+TARGET_KERNEL_SOURCE := kernel/huawei/hi6250-8
+TARGET_KERNEL_CONFIG := merge_hi6250_defconfig
+
+
 # Inherit the proprietary files
 include vendor/huawei/prague/BoardConfigVendor.mk
