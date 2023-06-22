e1e60a1f5dbc9122bebc7d0370b1d9ef2b9cd701
Giovanni Ricca <giovanniricca@proton.me>
2023-06-22 20:21:50 +0200

    prague: Build Gatekeeper stack
    Change-Id: I320d8429f9cf4bcdb7b0b563834b7de37a65f354

--- device.mk
+++ device.mk
@@ -92,6 +92,11 @@ PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl \
     android.hardware.gnss@1.0-service
 
+# Gatekeeper
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-impl \
+    android.hardware.gatekeeper@1.0-service
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
