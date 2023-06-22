a81f176882e896121440d709a4a4e0b930c58ef4
Giovanni Ricca <giovanniricca@proton.me>
2023-06-22 20:04:42 +0200

    prague: Build bluetooth stack
    Change-Id: If179e4845231db73ae8eeec18c43ab7d211ef49b

--- device.mk
+++ device.mk
@@ -60,6 +60,11 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0-impl \
+    android.hardware.bluetooth@1.0-service
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
