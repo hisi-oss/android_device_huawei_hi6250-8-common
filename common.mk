339c53510d003c5f7265005c61aa14bb6d4a8140
Giovanni Ricca <giovanniricca@proton.me>
2023-07-15 19:03:42 +0200

    hi6250-8-common: Build AOSP NFC service
    Change-Id: I2383f763578d74f281e951af6ee3c2b2c04991a6

--- common.mk
+++ common.mk
@@ -194,7 +194,11 @@ PRODUCT_PACKAGES += \
 
 # Nfc
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.0.vendor
+    android.hardware.nfc@1.2-service
+
+PRODUCT_PACKAGES += \
+    com.android.nfc_extras \
+    Tag
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf
