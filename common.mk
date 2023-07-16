18a7e57ad528ae5790cc7ab196f187ac9d2da407
Giovanni Ricca <giovanniricca@proton.me>
2023-07-16 17:37:49 +0200

    hi6250-8-common: Extract overlays
    * From PRA-LX1-user 8.0.0 HUAWEIPRA-LX1 384(C432) release-keys
    
    Change-Id: Idff98453385dcd9a840e24fc12fbaee74b47cd47

--- common.mk
+++ common.mk
@@ -271,6 +271,10 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf
 
+PRODUCT_PACKAGES += \
+    WifiOverlay \
+    TetheringConfigOverlay
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf
