8d059b3b001e1ba1731f77d1a9b25ad4b0be84f0
Giovanni Ricca <giovanniricca@proton.me>
2023-08-28 22:29:27 +0200

    hi6250-8-common: Build AOSP vibrator service
    Change-Id: I9c5f0b9e8192052e5a2172b82de0089398983e79

--- common.mk
+++ common.mk
@@ -276,7 +276,8 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0.vendor
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.vibrator@1.0-service
 
 # VNDK
 PRODUCT_PACKAGES += \
