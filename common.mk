1bd955356be82803e829dcff673c5f390620e2bd
Giovanni Ricca <giovanniricca@proton.me>
2023-09-03 14:47:48 +0200

    hi6250-8-common: Import powerhint for hi6250 (EMUI 8)
    * Mostly based on perfgenius boost config
    
    Change-Id: I008385a4673a226d495b14b9673b57794b77212e

--- common.mk
+++ common.mk
@@ -232,6 +232,9 @@ PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/cgroups_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
     system/core/libprocessgroup/profiles/task_profiles_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # Recovery
 PRODUCT_PACKAGES += \
     init.recovery.hi6250.rc
