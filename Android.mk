cddafcb6e10fa2d6e276b7b68a77bc14ac00e5e1
Giovanni Ricca <giovanniricca@proton.me>
2024-01-07 17:37:44 +0100

    hi6250-8-common: berlin -> bln
    Change-Id: Ifdbc23c6369f23fa0bf9cf5420170b38d49a55e0

--- Android.mk
+++ Android.mk
@@ -6,7 +6,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter berlin prague warsaw,$(TARGET_DEVICE)),)
+ifneq ($(filter bln prague warsaw,$(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -25,7 +25,7 @@ source "${HELPER}"
 setup_vendor "${DEVICE_COMMON}" "${VENDOR_COMMON:-$VENDOR}" "${ANDROID_ROOT}" true
 
 # Warning headers and guards
-write_headers "berlin prague warsaw"
+write_headers "bln prague warsaw"
 
 # The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
