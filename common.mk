28c9850cd92100d4dcb8c571c79bd76a0f9fd633
R0rt1z2 <me@r0rt1z2.com>
2024-01-30 20:07:32 +0100

    hi6250-8-common: Configure Keymaster stack
    * Uses stock keymaster impl

--- common.mk
+++ common.mk
@@ -197,10 +197,10 @@ PRODUCT_PACKAGES += \
 
 # Keymaster
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service
 
 PRODUCT_PACKAGES += \
+    libkeymaster3device.vendor \
     libkeystore-engine-wifi-hidl \
     libkeystore-wifi-hidl
 
