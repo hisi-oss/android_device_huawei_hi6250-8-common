a18ed7ad246da4a8d77308c33d9bc47275e8459d
Emil <emilwagenplast@gmail.com>
2024-08-04 17:41:33 +0200

    hi6250-8-common: Switch to AOSP Sensors service

--- common.mk
+++ common.mk
@@ -280,10 +280,12 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0.vendor
+    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-service
 
 PRODUCT_PACKAGES += \
-    android.frameworks.sensorservice@1.0.vendor
+    android.frameworks.sensorservice@1.0.vendor \
+    vendor.huawei.hardware.sensors@1.0.vendor
 
 # Shims
 PRODUCT_PACKAGES += \
